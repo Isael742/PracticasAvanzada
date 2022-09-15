ace0a959809e15c7d8a74f7fa5b6eb0a40b7216c
Isael742 <62968411+Isael742@users.noreply.github.com>
2022-09-15 09:56:40 -0600

    Less Basico

---
+++ Less Basico/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="public/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <form>
+            <fieldset>
+                <legend>
+                    Datos de acceso
+                </legend>
+
+                <label>
+                    Email
+                </label>
+                <input type="text" name="">
+
+                <label>
+                    Password
+                </label>
+                <input type="password" name="">
+
+                <button>
+                    Sumit
+                </button>
+            </fieldset>
+        </form>
+    </div>
+    
+</body>
+</html>
