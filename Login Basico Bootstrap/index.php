9b32e0a037b5e13aa845583ab6957c362cbebfd5
Isael742 <62968411+Isael742@users.noreply.github.com>
2022-09-20 10:59:25 -0500

    Add files via upload

---
+++ Login Basico Bootstrap/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+   
+    <!--link rel="stylesheet" type="text/css" href="public/css/main.css"-->
+</head>
+<body>
+    <div class="container text-center">
+                    <legend>
+                        Datos de acceso
+                    </legend>
+        <div class="d-flex justify-content-center">
+            <form>
+                <fieldset>
+                    <br>
+                    <label>
+                        Email
+                    </label>
+                    <input type="text" name="" placeholder="Email">
+
+                    <label>
+                        Password
+                    </label>
+                    <input type="password" name="" placeholder="Password">
+
+                    <button>
+                        Sumit
+                    </button>
+                </fieldset>
+            </form>
+        </div>
+            
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
