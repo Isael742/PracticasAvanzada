69a223c1ef9f31ce9ae6d5f71187f65e193b314b
Isael742 <isaelkeki107@hotmail.com>
2022-10-18 09:56:41 -0600

    Controladores de Laravel

--- LaravelProyect/routes/web.php
+++ LaravelProyect/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users/', [UserController::class, 'index']);
+
+Route::get('users/create', [UserController::class, 'create']);
+
+Route::get('users/{id}', [UserController::class, 'show']);
+
+Route::post('users/', [UserController::class, 'store']);
 
 Route::get('saludo/{name}',function($name){
     echo "HOLA ".$name;
